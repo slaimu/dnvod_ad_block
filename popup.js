dfba969753545c86ac51b99ea7e8c395f4725366
xiaowu zhang <xiaowu.zhangv@gmail.com>
2017-11-30 20:41:27 +0100

    more paths

--- popup.js
+++ popup.js
@@ -13,6 +13,12 @@ chrome.webRequest.onBeforeRequest.addListener (
         "http://static.dnvod.tv/upload/gaming/*",
         "http://s1-a1.dnvod.tv/*",
         "http://s1-a2.dnvod.tv/*",
-        "http://s1-a3.dnvod.tv/*"]},
+        "http://s1-a3.dnvod.tv/*",
+        "http://s1-a4.dnvod.tv/*",
+        "http://s1-a5.dnvod.tv/*",
+        "http://s1-a6.dnvod.tv/*",
+        "http://s1-a7.dnvod.tv/*",
+        "http://s1-a8.dnvod.tv/*",
+        "http://s1-a9.dnvod.tv/*",]},
     ["blocking"]
 );
